0821ea93600d5d33d9a46b159ddaa94fb1a3b840
Samet USTAOGLU <ustaoglusamet05@gmail.com>
2021-08-05 07:44:57 +0300

    artık yıl düzenlendi

--- leap-year.py
+++ leap-year.py
@@ -2,8 +2,9 @@ year=int(input("Enter year to be checked:"))
 if(year%4==0 and year%100!=0 or year%400==0):
    
     print("THIS IS JUST A LEAP YEAR!)
+    print("bu bir artık yıl")
 else:
     print("The year isn't a leap year!)
-          
+    print("bu bir artık yıl değil")
           
           hello
